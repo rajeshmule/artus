1282231cc52c487ad06bc7ae19708ee306e85c5c
Rajesh mule <rajeshsmule@gmail.com>
2020-03-16 14:48:08 +0530

    style signup

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="hero is-primary">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <h1 className="title">artus</h1>
+          <h1 className="title is-1">artus</h1>
           <h2 className="subtitle">A place to share your knowledge.</h2>
         </div>
       </div>
