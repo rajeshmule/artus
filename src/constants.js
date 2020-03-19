7cd95c47b2d5766dcc297ed1a51724a8ea5d566f
Rajesh mule <rajeshsmule@gmail.com>
2020-03-19 14:50:44 +0530

    add dev api url to localhost:3000

--- src/constants.js
+++ src/constants.js
@@ -11,4 +11,9 @@ const dev = {
     API_URL: "https://conduit.productionready.io/api"
   }
 };
+// const dev = {
+//   url: {
+//     API_URL: "http://localhost:3000/api/v1"
+//   }
+// };
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
