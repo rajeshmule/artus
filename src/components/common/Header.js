99c699f3ff08c76213da1c786b8ac3892fe94b20
Rajesh mule <rajeshsmule@gmail.com>
2020-05-22 12:56:23 +0530

    navbar in mobile view

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,30 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
 function Header(props) {
   return (
     <nav
@@ -18,7 +42,7 @@ function Header(props) {
           className="navbar-burger burger"
           aria-label="menu"
           aria-expanded="false"
-          data-target="navbarBasicExample"
+          data-target="navMenu"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -26,7 +50,7 @@ function Header(props) {
         </Link>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div id="navMenu" className="navbar-menu">
         <div className="navbar-end">
           <div className="navbar-item">
             {props.isLoggedIn ? (
